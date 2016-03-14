09b3d50796e036be544716d6cf3e231911edcf4e
1182177 <1182177@A5S104MAC44.fib.upc.es>
2016-03-14 15:37:32 +0100

    Llibreries

---
+++ Programa/src/programa/Estat.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programa;
+
+import java.util.ArrayList;
+import IA.DistFS.*;
+
+/**
+ *
+ * @author Guillem
+ */
+
+public class Estat {
+    
+    private ArrayList <Integer> assignacio;
+    private ArrayList <Integer> ocupacioServidor;
+    
+    public Estat(Requests r) {
+        
+    }
+    
+}
