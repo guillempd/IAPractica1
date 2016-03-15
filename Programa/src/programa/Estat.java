0a3c9fe91c78770bf1cf8b83c9551619c1692ae1
pau.surrell <pau.surrell@a5s113pc35.fib.upc.es>
2016-03-15 12:44:40 +0100

    creadora admet diferents estats inicials

--- Programa/src/programa/Estat.java
+++ Programa/src/programa/Estat.java
@@ -19,16 +19,21 @@ public class Estat {
     
     private ArrayList <Integer> assignacio; //assignacio[i] = servidor
     private ArrayList <Integer> ocupacioServidor;
+    private Requests r;
+    private Servers s;
     private final int R;
     private final int S;
     
-    public Estat(Requests r, Servers s) {
+    public Estat(Requests r, Servers s, Generacio g) {
+        this.r = r;
+        this.s = s;
         R = r.size();
         S = s.size();
         
         for (int i = 0; i < S; i++){
             ocupacioServidor.add(0);
         }
+        
         //per cada request, li assignem un servidor random que contingui el fitxer
         for (int i = 0; i < R; ++i) { 
             
@@ -38,11 +43,37 @@ public class Estat {
             //id dels servidors que contenen el fitxer de la request
             
             Integer[] h = (Integer[]) set.toArray();
-            Random rand = new Random(System.currentTimeMillis());
-            int serv_triat = rand.nextInt(h.length);//triem un servidor aleatori
+            int serv_triat = escullServidor(user,h,g);
             assignacio.add(h[serv_triat]); //li assignem al servidor
             ocupacioServidor.set(serv_triat,s.tranmissionTime(serv_triat, user));
             //actualitzem el temps del servidor
+        }
+    }
+    
+    private int escullServidor(int user, Integer[] h, Generacio g){
+        if (g == Generacio.RANDOM){
+            Random rand = new Random(System.currentTimeMillis());
+            return rand.nextInt(h.length);//triem un servidor aleatori
+        }else if (g == Generacio.PROPER){
+            int mindist = s.tranmissionTime(h[0], user);
+            int minserv = h[0];
+            for (int i = 0; i < h.length; i++){
+                if (s.tranmissionTime(h[i], user) < mindist){
+                    mindist = s.tranmissionTime(h[i], user);
+                    minserv = h[i];
+                }
+            }
+            return minserv;
+        }else{ //g = llunya
+            int maxdist = s.tranmissionTime(h[0], user);
+            int maxserv = h[0];
+            for (int i = 0; i < h.length; i++){
+                if (s.tranmissionTime(h[i], user) > maxdist){
+                    maxdist = s.tranmissionTime(h[i], user);
+                    maxserv = h[i];
+                }
+            }
+            return maxserv;
         }
     }
     
