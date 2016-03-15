bbc4675918f74fbbadf2eaa4bc4b537cee33a869
David <David@Lenovo>
2016-03-15 15:52:23 +0100

    He afegit el javadoc (em sembla que funciona) i avançat el successors

--- Programa/src/programa/Practica1SuccessorsHillClimbing.java
+++ Programa/src/programa/Practica1SuccessorsHillClimbing.java
@@ -5,13 +5,50 @@
  */
 package programa;
 
+import IA.DistFS.Requests;
+import IA.DistFS.Servers;
 import aima.search.framework.Successor;
 import aima.search.framework.SuccessorFunction;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
 /**
  *
  * @author guillem.perez.delgado
  */
+public class Practica1SuccessorsHillClimbing implements SuccessorFunction {
+    
+    private Requests r;
+    private Servers s;
+    
+    public ArrayList<Successor> getSuccessors(Object aState) {
+        Estat e = (Estat)aState;
+        ArrayList<Integer> assignacio = e.getAssignacio();
+        ArrayList ret = new ArrayList();
+        int R = r.size();
+        for (int i = 0; i < R; ++i) {
+            Set<Integer> setServers = s.fileLocations(r.getRequest(i)[1]);
+            Iterator<Integer> it = setServers.iterator();
+            while (it.hasNext()) {
+                int server = it.next();
+                if (server != assignacio.get(i)) {
+                    Estat nouEstatDEuropa = new Estat(e);
+                    nouEstatDEuropa.Operador(i, server);
+                }
+            }
+        }
+    }
+    
+}
+
+
+/*
+package programa;
+
+import aima.search.framework.Successor;
+import aima.search.framework.SuccessorFunction;
+
 public class Practica1SuccessorsHillClimbing implements SuccessorFunction {
     
     public List getSuccessors(Object aState) {
@@ -21,3 +58,4 @@ public class Practica1SuccessorsHillClimbing implements SuccessorFunction {
     }
     
 }
+*/
