d1345a5a97f184df67dcb055c4be072c9d023c99
David <David@Lenovo>
2016-03-31 20:17:14 +0200

    fet

--- Programa/src/Practica1/Successors/SuccessorsSimulatedAnnealing.java
+++ Programa/src/Practica1/Successors/SuccessorsSimulatedAnnealing.java
@@ -5,10 +5,50 @@
  */
 package Practica1.Successors;
 
+import IA.DistFS.Requests;
+import IA.DistFS.Servers;
+import Practica1.Estat;
+import aima.search.framework.HeuristicFunction;
+import aima.search.framework.Successor;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
+
 /**
  *
  * @author David
  */
 public class SuccessorsSimulatedAnnealing {
+    
+    private Requests r;
+    private Servers s;
+    private HeuristicFunction hf;
+    
+    public SuccessorsSimulatedAnnealing (Requests r, Servers s, HeuristicFunction hf) {
+        this.r = r;
+        this.s = s;
+        this.hf = hf;
+    }
+    
+    public ArrayList<Successor> getSuccessors(Object aState) {
+        Estat e = (Estat)aState;
+        ArrayList<Integer> assignacio = e.getAssignacio();
+        ArrayList<Successor> ret = new ArrayList();
+        Random rand = new Random(System.currentTimeMillis());
+        int i = rand.nextInt(r.size()-1);
+            int fileID = r.getRequest(i)[1];
+            Set<Integer> setServers = s.fileLocations(fileID);
+            Integer[] servers = new Integer[setServers.size()];
+            setServers.toArray(servers);
+            int j;
+            do j = rand.nextInt(servers.length-1);
+            while (servers[j].equals(assignacio.get(i)));
+                Estat nouEstatDEuropa = new Estat(e);
+                nouEstatDEuropa.Operador(i, servers[j]);
+                ret.add(new Successor("Request " + i + " -> servidor " + servers[j] + ". Heuristica = " + hf.getHeuristicValue(nouEstatDEuropa) +"\n", nouEstatDEuropa));
+
+        return ret;
+    }
     
 }
